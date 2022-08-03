e8f96ef9e5dc16414396e7d90074101e11427de3
Paritosh Tripathi <tripathiparitosh935@gmail.com>
2022-08-03 17:00:11 +0530

    added map

--- pages/india.py
+++ pages/india.py
@@ -1,3 +1,4 @@
+from enum import auto
 from operator import imod
 from dash import Dash, html, dcc
 from pyparsing import White
@@ -9,30 +10,68 @@ from dash import html, dcc, callback, Input, Output
 from urllib.request import urlopen
 import json
 import plotly.express as px
-import xlrd
 import random
 import pandas as pd
 import seaborn as sns
 import geopandas as gpd
 import matplotlib.pyplot as plt
-
-
-fp = "Igismap/Indian_States.shp"
-map_df = gpd.read_file(fp)
-
-
+import numpy as np
+import plotly.io as pio
+pio.renderers.default = 'browser'
 dash.register_page(__name__)
+
+
 
 colors = {
     'background': '#FFF4E4',
     'text': '#000000'
 }
 
+india_states = json.load(open("maps/states_india.geojson", "r"))
+
+state_id_map = {}
+for feature in india_states["features"]:
+    feature["id"] = feature["properties"]["state_code"]
+    state_id_map[feature["properties"]["st_nm"]] = feature["id"]
+
+
+df = pd.read_csv("Data/india_census.csv")
+df["Density"] = df["Density[a]"].apply(lambda x: int(x.split("/")[0].replace(",", "")))
+df["id"] = df["State or union territory"].apply(lambda x: state_id_map[x])
+
+df["DensityScale"] = np.log10(df["Density"])
+
+fig = px.choropleth(
+    df,
+    locations="id",
+    geojson=india_states,
+    color="DensityScale",
+    hover_name="State or union territory",
+    hover_data=["Density"],
+    title="India Population Density",
+    height=800,
+    width=700
+)
+
+fig.update_geos(fitbounds="locations", visible=False)
+
+
+#fig.update_layout(margin=dict(t=10, b=10, r=10, l=10))
+
+fig.update_layout(
+    autosize=False
+)
+
 # define the page content with variable called layout
 layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children=''),
 
     html.Div(children='''
-        Dash: A web application framework for your data.
-    ''')
+       '''),
+    dcc.Graph(
+        id='india-map',
+        figure=fig,
+        responsive=True
+    )
 ])
+
